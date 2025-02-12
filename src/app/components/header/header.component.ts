ba0518c667138267495db206c5c22738d143eece
Italo Cruz <italocruz544@gmail.com>
2025-02-12 00:59:54 -0300

    fix: import header comonent

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgbCollapseModule, RouterLink],
+  imports: [NgbCollapseModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
