25b5450584a872afba2d2c371aff4ceadc764b0f
Italo Cruz <italocruz544@gmail.com>
2025-02-12 00:40:26 -0300

    feat: add collapse on navbar header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [NgbCollapseModule, RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  isCollapsed = true;
+
+  toggleMenu() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
