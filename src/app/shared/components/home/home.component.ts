07418cbd5d35c890bf3f6b76cb064ba179b9b627
Italo Cruz <italocruz544@gmail.com>
2025-02-13 03:10:13 -0300

    feat: add action component

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from '../../../components/about/about.component';
+import { ActionComponent } from "../../../components/action/action.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, AboutComponent],
+  imports: [CommonModule, AboutComponent, ActionComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
