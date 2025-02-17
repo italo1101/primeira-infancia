0238162694adb161f3984380db9770025ecd9275
Italo Cruz <italocruz544@gmail.com>
2025-02-17 03:16:01 -0300

    feat: add lab component and change SEO tags

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from '../../../components/about/about.component';
 import { ActionComponent } from "../../../components/action/action.component";
+import { LabComponent } from "../../../components/lab/lab.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, AboutComponent, ActionComponent],
+  imports: [CommonModule, AboutComponent, ActionComponent, LabComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
